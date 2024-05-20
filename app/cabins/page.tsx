6f34e182b1cb8d2017bee2a3bc874fe4fac09801
amejid <abdelmejid89@gmail.com>
2024-05-20 12:08:15 +0300

    Implement data fetching

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-  return <h1>Cabins Page</h1>;
+import Counter from "@/app/components/Counter";
+
+export type User = { id: number; name: string };
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await res.json();
+
+  return (
+    <>
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user: User) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </>
+  );
 }
