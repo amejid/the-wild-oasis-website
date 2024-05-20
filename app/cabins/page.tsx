5a71233f82725b487ff2850ca51285e6e5a67868
amejid <abdelmejid89@gmail.com>
2024-05-20 16:47:31 +0300

    Configure tailwind and styles

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,21 +1,11 @@
-import Counter from "@/app/components/Counter";
+export const metadata = {
+  title: "Cabins",
+};
 
-export type User = { id: number; name: string };
-
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-
-  const data = await res.json();
-
+export default function Page() {
   return (
-    <>
+    <div>
       <h1>Cabins Page</h1>
-      <ul>
-        {data.map((user: User) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
-    </>
+    </div>
   );
 }
--- app/components/Counter.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useState } from "react";
-import { User } from "@/app/cabins/page";
-
-export default function Counter({ users }: { users: User[] }) {
-  const [count, setCount] = useState(0);
-
-  return <button onClick={() => setCount((c) => c + 1)}>{count}</button>;
-}
