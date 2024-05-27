34bd4243c3f54436df029dead446fc0c1ea53b6f
amejid <abdelmejid89@gmail.com>
2024-05-27 10:47:16 +0300

    Implement deleting a reservation

--- app/_lib/actions.ts
+++ app/_lib/actions.ts
@@ -3,6 +3,7 @@
 import { auth, CustomSession, signIn, signOut } from "@/app/_lib/auth";
 import { supabase } from "@/app/_lib/supabase";
 import { revalidatePath } from "next/cache";
+import { getBookings } from "@/app/_lib/data-service";
 
 export async function signInAction() {
   await signIn("google", {
@@ -42,4 +43,31 @@ export async function updateGuest(formData: FormData) {
   }
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId: number) {
+  const session = (await auth()) as CustomSession;
+
+  if (!session) {
+    throw new Error("You must be logged in");
+  }
+
+  const guestBookings = await getBookings(session.user.guestId ?? 0);
+
+  const guestBookingIds = guestBookings?.map((booking) => booking.id);
+
+  if (!guestBookingIds?.includes(bookingId)) {
+    throw new Error("You are not allowed to delete this booking");
+  }
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) {
+    throw new Error("Booking could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
 }
--- app/_lib/data-service.ts
+++ app/_lib/data-service.ts
@@ -205,16 +205,3 @@ export async function updateBooking(id: number, updatedFields) {
   }
   return data;
 }
-
-/////////////
-// DELETE
-
-export async function deleteBooking(id: number) {
-  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
-
-  if (error) {
-    console.error(error);
-    throw new Error("Booking could not be deleted");
-  }
-  return data;
-}
